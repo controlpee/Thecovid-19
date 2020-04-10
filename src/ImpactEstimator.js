0ca8c7e55f1686ed2c3a6c9df3d204db0541f2a2
Pius E. Uduma <41404729+controlpee@users.noreply.github.com>
2020-04-10 04:24:39 +0100

    initial commit

--- src/ImpactEstimator.js
+++ src/ImpactEstimator.js
@@ -1,41 +1,44 @@
-const covid19ImpactEstimator = {
-    
+[
+  {
     "data": {
 
-      "region": {
-          "name": 'Africa',
-          "avgAge": 19.7,
-          "avgDailyIncomeInUSD": 4,
-          "avgDailyIncomePopulation": 0.73
-      },
-"periodType": 'days',
-"timeToElapse": 38,
-"reportedCases": 2747,
-"population": 92931687,
-"totalHospitalBeds": 678874
-    },
-
-"estimate": {
-    "impact": {
-
-        "currentlyInfected": 27470,
-        "infectionsByRequestedTime": 112517120,
-        "severeCasesByRequestedTime": 16877568,
-        "hospitalBedsByRequestedTime": -16639962,
-              "casesForICUByRequestedTime": 5625856,
-              "casesForVentilatorsByRequestedTime": 2250342,
-              "dollasInFlight": 12484899635.2
-          },
+        "region": {
+                    "name": 'Africa',
+                    "avgAge": 19.7,
+                    "avgDailyIncomeInUSD": 4,
+                    "avgDailyIncomePopulation": 0.73
           
-          "severeImpact": {
-          "currentlyInfected": 137350,
-          "infectionsByRequestedTime": 562585600,
-          "severeCasesByRequestedTime": 84387840,
-          "hospitalBedsByRequestedTime": -84150234,
-          "casesForICUByRequestedTime": 28129280,
-          "casesForVentilatorsByRequestedTime": 11251712,
-          "dollarsInFlight": 62424498176
-            }
-}
-
-  }
+                },
+          "periodType": 'days',
+          "timeToElapse": 38,
+          "reportedCases": 2747,
+          "population": 92931687,
+          "totalHospitalBeds": 678874
+              },
+          
+          "estimate": {
+         "impact": {
+          
+                  "currentlyInfected": 27470,
+                  "infectionsByRequestedTime": 112517120,
+                  "severeCasesByRequestedTime": 16877568,
+                  "hospitalBedsByRequestedTime": -16639962,
+                        "casesForICUByRequestedTime": 5625856,
+                        "casesForVentilatorsByRequestedTime": 2250342,
+                        "dollasInFlight": 12484899635.2
+                    },
+                    
+                    "severeImpact": {
+                    "currentlyInfected": 137350,
+                    "infectionsByRequestedTime": 562585600,
+                    "severeCasesByRequestedTime": 84387840,
+                    "hospitalBedsByRequestedTime": -84150234,
+                    "casesForICUByRequestedTime": 28129280,
+                    "casesForVentilatorsByRequestedTime": 11251712,
+                    "dollarsInFlight": 62424498176
+                      }
+          }
+  }  
+    
+  
+]
