69091b82532a1567ae214aed2432410174b6e751
Pius E. Uduma <41404729+controlpee@users.noreply.github.com>
2020-04-11 11:35:58 +0100

    initial commit

--- src/estimator.js
+++ src/estimator.js
@@ -8,8 +8,8 @@ avgAge: 19.7,
 avgDailyIncomeInUSD: 4,
 avgDailyIncomePopulation: 0.73    
 },
-periodType: 'weeks',
-timeToElapse: 1,
+periodType: 'days',
+timeToElapse: 38,
 reportedCases: 2747,
 population: 92931687,
 totalHospitalBeds: 678874
